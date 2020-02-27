8a2de2fe71e672d59b95ab9c922a34a7980d87c6
gunkim0318 <gunkim0318@gmail.com>
2020-02-27 17:26:06 +0900

    화면 구성

--- src/main/java/SpringBoot/Security/config/SecurityConfig.java
+++ src/main/java/SpringBoot/Security/config/SecurityConfig.java
@@ -1,12 +1,35 @@
-//package SpringBoot.Security.config;
-////
-////import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-////import org.springframework.stereotype.Component;
-////
-////@Component
-////public class SecurityConfig extends WebSecurityConfigurerAdapter {
-////    @Override
-////    protected void configure(HttpSecurity http) throws Exception {
-////    }
-////}
+package SpringBoot.Security.config;
+
+import SpringBoot.Security.config.Service.SecurityService;
+import SpringBoot.Security.domain.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private SecurityService securityService = null;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/security/user1").hasRole(Role.USER.name())
+                .antMatchers("/security//admin1").hasRole(Role.ADMIN.name())
+                .antMatchers("/security//admin2").hasRole(Role.ADMIN.name())
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("id")
+                .passwordParameter("pw").permitAll();
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+}
---
+++ src/main/java/SpringBoot/Security/config/Service/SecurityService.java
@@ -0,0 +1,4 @@
+package SpringBoot.Security.config.Service;
+
+public class SecurityService {
+}
